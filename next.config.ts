5b53328011262e2f5bdbfe4053fc7ae2df321487
elinnov-jasondelapena <jason.delapena@elinnovtech.com>
2026-03-31 22:05:28 +0800

    fixed display issue

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self'",
+              "script-src 'self' 'unsafe-inline'",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob:",
               "connect-src 'self' https://*.supabase.co https://www.googleapis.com",
